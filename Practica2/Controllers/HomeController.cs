b24befba7f822063255a8c64b11516e1d5cfc189
JaredDiaz26 <171g0223@rcarbonifera.tecnm.mx>
2020-09-17 15:50:57 -0500

    Version 1

---
+++ Practica2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practica2.Models;
+
+namespace Practica2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+
+            return View(estados);
+        }
+    }
+}
